9d2cf3b499fa7dfa447dae175df176ca4d4e8ca2
Hyuntae Park <proHyuntae@gmail.com>
2023-12-08 22:38:50 +0900

    Update newton-raphson

--- differentiation.py
+++ differentiation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import typing
+from numpy import typing as nptyping
 
 
 def diff(func: typing.Callable[[float], float], x: float):
@@ -9,3 +10,23 @@ def diff(func: typing.Callable[[float], float], x: float):
     increment = 1e-5
     result = (func(x + increment) - func(x)) / increment
     return result
+
+
+def jacobi(
+    func: typing.Callable[[nptyping.ArrayLike], nptyping.ArrayLike],
+    x: nptyping.ArrayLike,
+) -> np.ndarray:
+    """
+    Return numerically calculated Jacobian of given vector function at vector x.
+    """
+    num_var = len(x)
+    num_eqn = len(func(x))
+    jacobian = np.zeros((num_eqn, num_var))
+    increment = 1e-5
+
+    for i in range(num_var):
+        x_plus_inc = x.copy()
+        x_plus_inc[i] += increment
+        jacobian[:, i] = (func(x_plus_inc) - func(x)) / increment
+
+    return jacobian
